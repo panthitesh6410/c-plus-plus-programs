e5fa859e8efdb6c423d85591841b5c57df3355a7
panthitesh6410 <panthitesh6410@gmail.com>
2019-11-10 16:57:35 +0530

    adding new programs

--- problem28.cpp
+++ problem28.cpp
@@ -1,1 +1,12 @@
-// program to accept string containing consecutive 1's :
+// program to find the longest possible palindrome sub-string in a given string.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str;
+    cin>>str;
+    
+
+    return 0;
+}
