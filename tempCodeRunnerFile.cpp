5dda659468eb43b2b22472ddf48efe172f418ab9
panthitesh6410 <panthitesh6410@gmail.com>
2020-03-19 17:24:42 +0530

    codeforces problems

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,2 @@
-#include<iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cin>>n;
-//     int num = 2;
-//     for(int i=1;i<=n;i++)
-//     {
-//         cout<<num<<" ";
-//         num *= 3;
-//     }
-//     return 0;
-// }
+();i++)
+    //     cout<<v[i]<<endl
